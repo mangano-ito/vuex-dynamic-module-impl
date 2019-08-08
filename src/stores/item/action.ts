63722c5e47f4e94b8d3276706d43293c0acf6436
Mangano Ito <47137677+mangano-ito@users.noreply.github.com>
2019-08-08 17:07:56 +0900

    add item store implementation

---
+++ src/stores/item/action.ts
@@ -0,0 +1,6 @@
+export enum Actions {
+    INIT = 'INIT',
+    UPVOTE = 'UPVOTE',
+    DOWNVOTE = 'DOWNVOTE',
+}
+
---
+++ src/stores/item/state.ts
@@ -0,0 +1,4 @@
+export default interface State {
+    id: number;
+    likes: number;
+}
